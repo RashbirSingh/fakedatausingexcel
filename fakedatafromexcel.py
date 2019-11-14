81770409ae061cb1e46d2e91b13c1e18345618e4
Rashbir Singh <rashbits@gmail.com>
2019-11-14 17:18:52 +0530

    Adding code and output files

---
+++ fakedatafromexcel.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  7 14:31:51 2019
+
+@author: ubuntu
+"""
+
+from faker import Faker
+import pandas as pd
+import numpy as np
+import random
+import re
+import os
+
+class fakedata:
+    
+    def normal(self, repeat=100, loc=1, scale=100):
+        datalist=[]
+        for i in range(repeat):
+            datalist.append(abs(int(np.random.normal(loc, scale))))
+        return datalist
+    
+    def fakedatagenarator(self, repeat=1000, start_id=1, path='output'):
+        fake = Faker()
+        df_new=pd.DataFrame()
+        xl = pd.ExcelFile("Related.XLSX")
+        sheetsList = xl.sheet_names
+        sheetsList.remove('List Table')
+        
+        for sheet in sheetsList:
+            df = xl.parse(sheet)
+            for row in range(len(df.iloc[:, 1])):
+                FieldName, DataType = df.iloc[row, 0].upper(), re.sub(r'[^A-z]', '', df.iloc[row, 1].upper())
+                if DataType=='NUMBER':
+                    if FieldName == 'AGE':
+                        df_new[FieldName]=[random.choice(list(range(1,100))) for i in range(repeat)]
+                    else:
+                        df_new[FieldName]=self.normal(repeat)
+            
+                elif DataType=='TIMESTAMP' or DataType=='DATE':
+                    df_new[FieldName]=[fake.date() for i in range(repeat)]
+            
+                elif DataType=='VARCHAR':
+                    if FieldName == 'SEX' or FieldName == 'GENDER':
+                        df_new[FieldName]=[random.choice(['M', 'F']) for i in range(repeat)]
+                    elif FieldName == 'AREA_CODE' or FieldName =='PINCODE':
+                        df_new[FieldName]=[fake.profile()['residence'].split()[-1] for i in range(repeat)]
+                    else:
+                        df_new[FieldName]=[fake.name() for i in range(repeat)]
+            
+            df_new.index=list(np.array(df_new.index)+start_id)
+            
+            if not os.path.exists(path):
+                os.makedirs(path)
+                
+            df_new.to_csv(path+'/'+sheet+'.csv', index=True, header=True)
+            df_new = pd.DataFrame()
---
+++ testfile.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 14 17:03:13 2019
+
+@author: ubuntu
+"""
+
+import time
+from fakedatafromexcel import fakedata
+start_time = time.time()
+fd = fakedata()
+fd.fakedatagenarator()
+end_time = time.time()
